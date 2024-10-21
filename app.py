706569ec2b7daab08982b611f3f3f0b84b738f44
Cynthia dos programa toppp <133544879+cynthprogramer@users.noreply.github.com>
2024-10-21 08:22:11 -0300

    flask inicio

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return ' <h1> olá <br> mundo </h1>'
+
+@app.route('/aluno/<nome>')
+def aluno(nome):
+    return nome
